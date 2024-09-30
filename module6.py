679d316708c48ae4c923daddc990644dba3f447d
bullseye000 <dks1231970@gmail.com>
2024-09-30 20:16:41 +0530

    Add files via upload

---
+++ module6.py
@@ -0,0 +1,23 @@
+#-------------------------------------------------------------------------------
+# Name:        module1
+# Purpose:
+#
+# Author:      dks12
+#
+# Created:     12-05-2024
+# Copyright:   (c) dks12 2024
+# Licence:     <your licence>
+#-------------------------------------------------------------------------------
+
+sub=("EEE")
+print('ill study today') if sub=='EEE' or sub=='C language' else print("will not study")
+
+
+sub=("english")
+study='will study' if sub=='chemistry' or sub=='english' else 'will not study'
+print(study)
+
+
+vote=('false','true')[5>18]
+print(vote)
+#vbsz
